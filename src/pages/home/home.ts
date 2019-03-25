62f41b73bd44f88a283c585001bfcd7b8e97733a
Michael Purdy <0mpurdy@gmail.com>
2019-03-25 17:17:38 +0000

    Working base64 version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,7 +27,7 @@ export class HomePage {
     const options: CameraOptions = {
       quality: 100,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
@@ -35,44 +35,13 @@ export class HomePage {
     this.camera.getPicture(options).then(async (imageData) => {
       this.imageData = imageData;
 
-      let photo = {} as any;
-      this.photoUrls = [];
+      // let photo = {} as any;
+      // this.photoUrls = [];
 
-      try {
-        const win = window as any;
-
-        const entry = await this.file.resolveLocalFilesystemUrl(imageData) as FileEntry;
-        console.log('Got entry', entry);
-        photo.original = entry;
-        this.photoUrls.push({ name: 'originalPath', url: entry.toURL() });
-        this.photoUrls.push({ name: 'originalSplit', url: entry.toURL().split('//')[1] });
-        this.photoUrls.push({ name: 'ionicNativeOriginal ', url: win.Ionic.WebView.convertFileSrc(entry.toURL()) });
-
-        const photoFile = await new Promise((resolve, reject) => {
-          entry.file(resolve, reject);
-        });
-        console.log('Got photo file', photoFile);
-
-        const tempDirectory = await this.file.resolveLocalFilesystemUrl(this.file.tempDirectory) as DirectoryEntry;
-        console.log('Got temp directory', tempDirectory);
-
-        const copyFileResult = await new Promise<Entry>((resolve, reject) => {
-          return entry.copyTo(tempDirectory, entry.name, resolve, reject);
-        });
-        console.log('Copied file', copyFileResult);
-        photo.copy = copyFileResult;
-        this.photoUrls.push({ name: 'copyNative', url: copyFileResult.nativeURL });
-        this.photoUrls.push({ name: 'copyNativeSplit', url: copyFileResult.nativeURL.split('//')[1] });
-        this.photoUrls.push({ name: 'ionicNativeCopy', url: win.Ionic.WebView.convertFileSrc(copyFileResult.nativeURL) });
-
-      } catch (err) {
-        console.log('Camera error inside promise: ', err);
-      } 
-      this.photo = photo;
 
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
-      // this.photo = 'data:image/jpeg;base64,' + imageData;
+      this.photo = 'data:image/jpeg;base64,' + imageData;
       console.log('Image data', imageData);
       console.log('Photo', this.photo);
     }, (err) => {
